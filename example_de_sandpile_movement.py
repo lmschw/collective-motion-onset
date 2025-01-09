0de4ee1d6227d63619fc153a9a9d6426b84e65b9
lmschw <lillyschwarzenbach@gmail.com>
2025-01-09 19:11:57 +0100

    update examples

--- example_de_sandpile_movement.py
+++ example_de_sandpile_movement.py
@@ -4,24 +4,62 @@ from simulators.sandpile_model.sandpile_model_movement import SandpileModel
 from enums.placement_type import PlacementTypePredator, PlacementTypePrey
 from enums.cell_visibility import CellVisibility
 from enums.predator_behaviour import PredatorBehaviour
+from enums.metrics import Metrics
+import services.service_logging as slog
+import services.service_helper as shelp
 
-evo = DifferentialEvolution(tmax=100,
-                            grid_size=(5, 5),
-                            density=None,
-                            num_particles=15,
-                            num_generations=20,
-                            num_iterations_per_individual=1,
-                            use_norm=True,
-                            population_size=30,
-                            bounds=[0,1],
-                            update_to_zero_bounds=[0,0],
-                            mutation_scale_factor=1,
-                            crossover_rate=0.5,
-                            early_stopping_after_gens=None,
-                            cell_visibility=CellVisibility.SQUARE_EIGHT,
-                            allow_stay=True,
-                            agents_per_cell_limit=2,
-                            placement_type_prey=PlacementTypePrey.RANDOM,
-                            placement_type_predator=PlacementTypePredator.RANDOM)
+tmax = 1000
+grid_size = (5,5)
+num_particles = None
+num_generations = 20
+num_iters = 10
+population_size = 30
+bounds = [0,1]
+zero_bounds = [0,0]
+cell_visibility = CellVisibility.SQUARE_EIGHT
+allow_stay = True
+early_stopping = None
+agent_per_cell_limit = 2
+placemenent_prey = PlacementTypePrey.RANDOM
+placement_predator = PlacementTypePredator.RANDOM
+num_predator = 1
+metric = Metrics.NUMBER_OF_SURVIVORS_AT_FINAL_TIMESTEP
 
-evo.run(save_path_plots="test_plots.jpeg", save_path_log="test_log.csv")
+for density in [0.3]:
+    postfix = f"_test_tmax={tmax}_grid={grid_size}_d={density}_n={num_particles}_cv={cell_visibility.value}_as={allow_stay}_lim={agent_per_cell_limit}_plp={placemenent_prey.value}_plh={placement_predator.value}_m={metric.value}"
+    save_path_best = f"best{postfix}.csv"
+    save_path_best_normalised = f"best{postfix}_normalised.csv"
+    save_path_general = f"all{postfix}"
+    save_path_plot = f"plot{postfix}"
+
+    slog.initialise_log_file_with_headers(slog.create_headers(len_weights=cell_visibility.number_cells, is_best=True), save_path=save_path_best)
+    slog.initialise_log_file_with_headers(slog.create_headers(len_weights=cell_visibility.number_cells, is_best=True), save_path=save_path_best_normalised)
+
+    for i in range(num_iters):
+        evo = DifferentialEvolution(tmax=tmax,
+                                grid_size=grid_size,
+                                density=density,
+                                num_particles=num_particles,
+                                num_generations=num_generations,
+                                num_iterations_per_individual=1,
+                                use_norm=True,
+                                population_size=population_size,
+                                bounds=bounds,
+                                update_to_zero_bounds=zero_bounds,
+                                early_stopping_after_gens=early_stopping,
+                                cell_visibility=cell_visibility,
+                                allow_stay=allow_stay,
+                                agents_per_cell_limit=agent_per_cell_limit,
+                                placement_type_prey=placemenent_prey,
+                                placement_type_predator=placement_predator,
+                                num_predators=num_predator,
+                                metric=metric)
+
+        
+        best = evo.run(save_path_log=save_path_general, save_path_plots=save_path_plot)
+        print(f"BEST overall: {best}")
+
+
+        slog.log_results_to_csv([{'iter': i, 'individual': np.array(best[0]), 'fitness': best[1]}], prepare=True, save_path=save_path_best)
+        slog.log_results_to_csv([{'iter': i, 'individual': shelp.normalise(np.array(best[0])), 'fitness': best[1]}], prepare=True, save_path=save_path_best_normalised)
+
--- example_sandpile_model_movement.py
+++ example_sandpile_model_movement.py
@@ -7,25 +7,60 @@ from neural_network.neural_network import NeuralNetwork
 from neural_network.activation_layer import ActivationLayer
 from neural_network.fully_connected_layer import FullyConnectedLayer
 import services.service_neural_network as snn
+import services.service_preparation as sprep
+from animation.animator import Animator
+from animation.animator_matplotlib import MatplotlibAnimator
+from animation.grid_animator import GridAnimator
+
+weights = np.array([0.78923, 0.,      0,      0.,      0,      0.02552, 1.,      0.     ])
+
+
+tmax = 1000
+grid_size = (5,5)
+density = 0.3
+num_particles = None
+num_generations = 20
+num_iters = 1
+population_size = 30
+bounds = [0,1]
+zero_bounds = [0,0]
+cell_visibility = CellVisibility.SQUARE_EIGHT
+allow_stay = True
+early_stopping = None
+agent_per_cell_limit = 2
+placemenent_prey = PlacementTypePrey.RANDOM
+placement_predator = PlacementTypePredator.RANDOM
+num_predator = 1
+
+if num_particles == None:
+    num_particles = sprep.get_number_of_particles_for_constant_density_for_grid(density=density, grid_size=grid_size, agents_per_cell_limit=agent_per_cell_limit)
+
 
 weights_size = 8
-weights = np.array([0.1, 0.1, 0.1, 0.2, 0.1, 0.1, 0.1, 0.2])
 nn = NeuralNetwork()
 fully_connected_layer = FullyConnectedLayer(input_size=weights_size, output_size=5)
 fully_connected_layer.set_weights(weights=weights)
 nn.add(fully_connected_layer)
 nn.add(ActivationLayer(activation=snn.tanh, activation_prime=snn.tanh_prime))
 
-model = SandpileModel(num_agents=5,
-                      grid_size=(5,5),
-                      model=nn,
-                      placement_type_prey=PlacementTypePrey.RANDOM,
-                      cell_visibility=CellVisibility.SQUARE_EIGHT,
-                      allow_stay=True,
-                      agents_per_cell_limit=2,
-                      num_predators=1,
-                      predator_behaviour=PredatorBehaviour.NEAREST_PREY,
-                      placement_type_predator=PlacementTypePredator.BORDER)
+model = SandpileModel(
+                                        num_agents=num_particles,
+                                        grid_size=grid_size,
+                                        model=nn,
+                                        placement_type_prey=placemenent_prey,
+                                        cell_visibility=cell_visibility,
+                                        allow_stay=allow_stay,
+                                        agents_per_cell_limit=agent_per_cell_limit,
+                                        num_predators=num_predator,
+                                        predator_behaviour=PredatorBehaviour.NEAREST_PREY,
+                                        placement_type_predator=placement_predator,
+)
+simulation_data = model.simulate(tmax=tmax)
 
-model.print_grid()
-model.simulate(tmax=5)
+animation_filename_base = "test"
+
+animation_filename = f"{animation_filename_base}"
+animator = MatplotlibAnimator(simulation_data, (grid_size[0],grid_size[1],100))
+preparedAnimator = animator.prepare(GridAnimator(), frames=tmax)
+preparedAnimator.setSimulationData(simulation_data=simulation_data, grid_size=grid_size, num_predators=num_predator)
+preparedAnimator.saveAnimation(f"{animation_filename}.mp4")
