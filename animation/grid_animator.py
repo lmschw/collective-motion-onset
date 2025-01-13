2ccb80b9c24b99e8d67893e7ea11a4d36db54164
Lilly Schwarzenbach <lillyschwarzenbach@gmail.com>
2025-01-13 14:27:07 +0100

    add grid lines too grid_animator

--- animation/grid_animator.py
+++ animation/grid_animator.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import animation
+from matplotlib.ticker import (AutoMinorLocator, MultipleLocator)
 from animation.animator import Animator
 import services.service_grid as sgrid
 
@@ -43,10 +44,13 @@ class GridAnimator(Animator):
             x, y = sgrid.get_x_y_for_cell_idx(self.placements[t][agent], self._domainSize)
             points[i][0] = x
             points[i][1] = y
-        #plt.plot(points[:,0],points[:,1],'ro')
-        #print(points)
         plt.xlim(-1, self._domainSize[0])
         plt.ylim(-1, self._domainSize[1])
-        plt.scatter(points[:,0], points[:,1], c=colours,  )
-        
+        plt.gca().xaxis.set_major_locator(MultipleLocator(self._domainSize[0]//5))
+        plt.gca().yaxis.set_major_locator(MultipleLocator(self._domainSize[1]//5))
+        plt.gca().xaxis.set_minor_locator(AutoMinorLocator(self._domainSize[0]//5 -1))
+        plt.gca().yaxis.set_minor_locator(AutoMinorLocator(self._domainSize[1]//5 -1))
+        plt.scatter(points[:,0], points[:,1], c=colours)
+        plt.gca().grid(which='major', color='#CCCCCC', linestyle='--')
+        plt.gca().grid(which='minor', color='#CCCCCC', linestyle=':')
         plt.title(f"$t$={t:.2f}")
